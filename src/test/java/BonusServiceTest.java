ccda6f8578fc158772fba7718b86c57d6b2766d2
kseniabobkova <bobkova@healbe.com>
2021-01-27 10:55:37 +0300

    Update

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -38,7 +38,7 @@ class BonusServiceTest {
     void shouldCalculateForUnregisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        long amount = 500_000_60;
+        long amount = 1_000_000_60;
         boolean registered = false;
         long expected = 500;
 
@@ -51,9 +51,9 @@ class BonusServiceTest {
     void shouldCalculateForUnregisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        long amount = 500_60;
+        long amount = 1000_60;
         boolean registered = false;
-        long expected = 5;
+        long expected = 10;
 
         long actual = service.calculate(amount, registered);
 
